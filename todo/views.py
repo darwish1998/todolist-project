1a1c0bd8b6e02b528ad2ad6bd8f3851e2fb21bf9
darwish1998 <darwish144.ad@gmail.com>
2021-09-24 22:49:00 +0300

    Almost finish the functionality

--- todo/views.py
+++ todo/views.py
@@ -5,6 +5,7 @@ from django.db import IntegrityError
 from django.contrib.auth import login ,logout , authenticate
 from .forms import TodoForm
 from .models import Todo
+from django.utils import timezone
 
 
 
@@ -59,9 +60,39 @@ def currenttodo(request):
     todos = Todo.objects.filter(user =request.user, datacompleted__isnull = True)
     return render(request, 'todo/currenttodo.html', {'todos':todos})
 
-def viewtodo(request,todo_pk):
-    todo = get_object_or_404(Todo, pk= todo_pk)
-    return render(request, 'todo/viewtodo.html',{'todo':todo})
+def completedtodos(request):
+    todos = Todo.objects.filter(user =request.user, datacompleted__isnull = False)
+    return render(request, 'todo/completedtodos.html', {'todos':todos})
+
+
+def viewtodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk= todo_pk, user = request.user)
+    if request.method == 'GET':
+        form = TodoForm(instance = todo )
+        return render(request, 'todo/viewtodo.html',{'todo':todo, 'form':form })
+    else:
+        try:
+            form = TodoForm(request.POST, instance = todo)
+            form.save()
+            return redirect('currenttodo')
+        except ValueError:
+            return render(request, 'todo/viewtodo.html', { 'todo':todo,  'form':form , 'error': 'Oops an error occur , please enter correct data format'})
+
+def completetodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'POST':
+        todo.datecompleted = timezone.now()
+        todo.save()
+        return redirect('currenttodo')
+
+
+
+def deletetodo(request,todo_pk):
+    todo = get_object_or_404(Todo, pk= todo_pk , user = request.user)
+    if request.method == 'POST':
+        todo.delete()
+        return redirect('currenttodo')
+
 
 def logoutuser(request):
     if request.method == 'POST':
--- todolist/urls.py
+++ todolist/urls.py
@@ -30,5 +30,9 @@ urlpatterns = [
     path('', views.home, name = 'home'),
     path('create/', views.createtodo, name = 'createtodo'),
     path('current/', views.currenttodo, name = 'currenttodo'),
-    path('todo/<int:todo_pk>', views.viewtodo, name = 'viewtodo'),
+    path('completed/', views.completedtodos, name = 'completedtodos'),
+    path('todo/<int:todo_pk>/', views.viewtodo, name = 'viewtodo'),
+    path('todo/<int:todo_pk>/delete', views.deletetodo, name = 'deletetodo'),
+     path('todo/<int:todo_pk>/complete', views.completetodo, name='completetodo'),
+
 ]
